009a8b180894b100e51f9b3a07b8939277146c62
chris <cmorrison82z@yahoo.com>
2023-12-08 00:49:40 +0000

    board

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -6,6 +6,9 @@ pub mod config {
     pub const SIZE: (f32, f32) = (1000., 1000.);
 }
 
+#[derive(Component)]
+pub struct Board;
+
 pub struct BoardPlugin;
 impl Plugin for BoardPlugin {
     fn build(&self, app: &mut App) {
@@ -13,13 +16,21 @@ impl Plugin for BoardPlugin {
     }
 }
 
-pub fn setup(mut cmd: Commands, textures: Res<TextureAssets>) {
-    cmd.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(config::SIZE.0, config::SIZE.1)),
+fn setup(mut cmd: Commands, textures: Res<TextureAssets>) {
+    cmd.spawn((
+        SpriteBundle {
+            // transform: Transform::IDENTITY.with_scale(Vec3::splat(4.)),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(config::SIZE.0, config::SIZE.1)),
+                ..Default::default()
+            },
+            texture: textures.card_king.clone(),
             ..Default::default()
         },
-        texture: textures.card_king.clone(),
-        ..Default::default()
-    });
+        Board,
+    ));
+}
+
+pub fn get_deck_position(board_size: Vec2) {
+
 }
