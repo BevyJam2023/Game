4197fadaf38ca56554c1a545b5ae6b8a443b432c
chris <cmorrison82z@yahoo.com>
2023-12-09 18:45:12 +0000

    im stupid

--- src/game_shapes/mod.rs
+++ src/game_shapes/mod.rs
@@ -1,4 +1,7 @@
-use bevy::{ecs::system::Command, prelude::{*, shape::RegularPolygon}};
+use bevy::{
+    ecs::system::Command,
+    prelude::{shape::RegularPolygon, *},
+};
 
 use crate::{loading::TextureAssets, AppState};
 
@@ -46,7 +49,7 @@ impl Into<RegularPolygon> for GamePolygon {
     fn into(self) -> RegularPolygon {
         RegularPolygon {
             radius: config::POLYGON_RADIUS,
-            sides: self.get_vertices().into()
+            sides: self.get_vertices().into(),
         }
     }
 }
@@ -58,23 +61,83 @@ impl GamePolygon {
             GamePolygon::Pentagon => 5,
             GamePolygon::Hexagon => 6,
         }
-}
+    }
 }
 
 pub struct GameShape {
     polygon: GamePolygon,
-    color: GameColor
+    color: GameColor,
+}
+impl GameShape {
+    fn get_bundle(self, ma: &Res<ShapeAssets>, c_m : &Res<ColorMaterialAssets>) -> ColorMesh2dBundle {
+        ColorMesh2dBundle {
+            mesh: get_polygon_mesh(&self.polygon, ma).into(),
+            material: get_color_material(&self.color, c_m),
+            ..Default::default()
+        }
+    }
 }
 
-pub struct 
+#[derive(Resource, Default)]
+pub struct ShapeAssets {
+    pub triangle: Handle<Mesh>,
+    pub square: Handle<Mesh>,
+    pub pentagon: Handle<Mesh>,
+    pub hexagon: Handle<Mesh>,
+}
+
+#[derive(Resource, Default)]
+pub struct ColorMaterialAssets {
+    pub red: Handle<ColorMaterial>,
+    pub green: Handle<ColorMaterial>,
+    pub blue: Handle<ColorMaterial>,
+    pub yellow: Handle<ColorMaterial>,
+}
+
+pub fn get_polygon_mesh(p: &GamePolygon, ma: &Res<ShapeAssets>) -> Handle<Mesh> {
+    match *p {
+        GamePolygon::Triangle => ma.triangle.clone_weak(),
+        GamePolygon::Square => ma.square.clone_weak(),
+        GamePolygon::Pentagon => ma.pentagon.clone_weak(),
+        GamePolygon::Hexagon => ma.hexagon.clone_weak(),
+    }
+}
+
+pub fn get_color_material(p: &GameColor, c_m : &Res<ColorMaterialAssets>) -> Handle<ColorMaterial> {
+    match *p {
+        GameColor::Red => c_m.red.clone_weak(),
+        GameColor::Green => c_m.green.clone_weak(),
+        GameColor::Blue => c_m.blue.clone_weak(),
+        GameColor::Yellow => c_m.yellow.clone_weak(),
+    }
+}
 
 pub struct GameShapePlugin;
 
 impl Plugin for GameShapePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, |mut cmd: Commands, mut a: ResMut<Assets<Mesh>>, m: ResMut<Assets<ColorMaterial>>| {
-            // TODO:
-            // Store Handles
-        });
+        app.insert_resource(ShapeAssets::default())
+            .insert_resource(ColorMaterialAssets::default())
+            .add_systems(
+                Startup,
+                |mut a: ResMut<Assets<Mesh>>,
+                 mut m: ResMut<Assets<ColorMaterial>>,
+                 mut s_a: ResMut<ShapeAssets>,
+                 mut c_m_a: ResMut<ColorMaterialAssets>| {
+                    s_a.triangle =
+                        a.add(shape::RegularPolygon::new(config::POLYGON_RADIUS, 3).into());
+                    s_a.square =
+                        a.add(shape::RegularPolygon::new(config::POLYGON_RADIUS, 4).into());
+                    s_a.pentagon =
+                        a.add(shape::RegularPolygon::new(config::POLYGON_RADIUS, 5).into());
+                    s_a.hexagon =
+                        a.add(shape::RegularPolygon::new(config::POLYGON_RADIUS, 6).into());
+
+                    c_m_a.red = m.add(ColorMaterial::from(RED));
+                    c_m_a.green = m.add(ColorMaterial::from(GREEN));
+                    c_m_a.blue = m.add(ColorMaterial::from(BLUE));
+                    c_m_a.yellow = m.add(ColorMaterial::from(YELLOW));
+                },
+            );
     }
 }
